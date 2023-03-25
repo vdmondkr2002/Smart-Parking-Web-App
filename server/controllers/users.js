77d72e9140010018c5065123f29d5a345b334d3e
vdmondkr2002 <vedantmondkar2002@gmail.com>
2023-03-26 00:49:09 +0530

    reduce sendOTP response time try2

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -7,62 +7,6 @@ const sendEmail = require('../Utils/sendEmail')
 const webpush = require('web-push')
 const { instance } = require("../Utils/razorPayInstance")
 
-const createUser = async (formData) => {
-
-    console.log("in create user")
-    try {
-
-        //Get the hashed password
-        // const salt = await bcrypt.genSalt(10)
-        // const hashedPassword = await bcrypt.hash(formData.password, salt)
-        const hashedPassword = passwordHash.generate(formData.password)
-
-        console.log("re")
-        //generate otp
-        const otpGenerated = generateOTP();
-
-        console.log("otp generated", otpGenerated)
-        //save the user in database
-
-
-        const newUser = await User.create({
-            email: formData.email, password: hashedPassword,
-            firstName: formData.firstName, lastName: formData.lastName,
-            userName: formData.userName, mobileNo: formData.mobileNo,
-            profilePic: formData.selectedImg,
-            createdAt: new Date().toISOString(),
-            otp: otpGenerated
-        })
-
-
-
-        if (!newUser) {
-            console.log("Here")
-            return { badMsg: "Unable To Sign Up" }
-        }
-
-        const subject = "[Smart Parking] Welcome smart parker"
-        const html = `
-        <div
-            class="container"
-            style="max-width: 90%; margin: auto; padding-top: 20px"
-        >
-            <h2>Welcome to the club</h2>
-            <h4> You are just one step away from becoming a smart parker</h4>
-            <p style="margin-bottom: 30px;">Pleas enter the sign up OTP to get started</p>
-            <h1 style="font-size: 40px; letter-spacing: 2px; text-align:center;">${otpGenerated}</h1>
-            <h5>If you haven't made this request. simply ignore the mail and no changes will be made</h5>
-        </div>`
-        const receiverMail = formData.email
-
-        await sendEmail({ html, subject, receiverMail })
-
-        return { goodMsg: "Created user" }
-    } catch (err) {
-        return { badMsg: "Failed to create" }
-    }
-
-}
 
 exports.sendOTP = async (req, res) => {
     req.body.otp = "1"
@@ -90,12 +34,42 @@ exports.sendOTP = async (req, res) => {
             return res.status(400).json({ msg: "Password don't match" })
         }
 
-        const newUser = await createUser(req.body)
+        const hashedPassword = passwordHash.generate(formData.password)
+        //generate otp
+        const otpGenerated = generateOTP();
 
-        if (newUser.badMsg) {
+        console.log("otp generated", otpGenerated)
+        //save the user in database
+
+
+        const newUser = await User.create({
+            email: formData.email, password: hashedPassword,
+            firstName: formData.firstName, lastName: formData.lastName,
+            userName: formData.userName, mobileNo: formData.mobileNo,
+            profilePic: formData.selectedImg,
+            createdAt: new Date().toISOString(),
+            otp: otpGenerated
+        })
+
+        if (!newUser) {
             return res.status(500).json({ msg: "Unable to sign up please try again later" })
         }
 
+        const subject = "[Smart Parking] Welcome smart parker"
+        const html = `
+        <div
+            class="container"
+            style="max-width: 90%; margin: auto; padding-top: 20px"
+        >
+            <h2>Welcome to the club</h2>
+            <h4> You are just one step away from becoming a smart parker</h4>
+            <p style="margin-bottom: 30px;">Pleas enter the sign up OTP to get started</p>
+            <h1 style="font-size: 40px; letter-spacing: 2px; text-align:center;">${otpGenerated}</h1>
+            <h5>If you haven't made this request. simply ignore the mail and no changes will be made</h5>
+        </div>`
+        const receiverMail = formData.email
+
+        await sendEmail({ html, subject, receiverMail })
         return res.status(200).json({ msg: "Account Created, Verify OTP Sent to your email id to access your account" })
     } catch (err) {
         return res.status(500).json({ msg: "Something went wrong.." })
