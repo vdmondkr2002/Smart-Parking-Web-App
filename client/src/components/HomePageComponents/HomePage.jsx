379fe2435b5f123a6512cf1b79a98da7a205317a
vdmondkr2002 <vedantmondkar2002@gmail.com>
2023-03-31 12:24:42 +0530

    Marker fix2

--- client/src/components/HomePageComponents/HomePage.jsx
+++ client/src/components/HomePageComponents/HomePage.jsx
@@ -95,8 +95,7 @@ const HomePage = () => {
     const navigate = useNavigate()
     const dispatch = useDispatch()
     const greenIcon = new L.Icon({
-        // iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
-        iconUrl: '../../images/marker-icon-2x-green.png',
+        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
         shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
         iconSize: [25, 41],
         iconAnchor: [12, 41],
